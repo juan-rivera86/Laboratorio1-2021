1d76537dc3225d105a03db52b12fca962aecfb03
jcrm86 <jcrm86@gmail.com>
2021-02-05 14:11:07 -0500

    CReacion de proyecto

---
+++ Laboratorio1-2021/src/laboratorio1/pkg2021/Laboratorio12021.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratorio1.pkg2021;
+
+/**
+ *
+ * @author juanr
+ */
+public class Laboratorio12021 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
